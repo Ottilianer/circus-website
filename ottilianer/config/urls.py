fea41c071aaf5009754292a23c0070cd533fada1
Leo Gall <gall.dev@proton.me>
2024-10-03 14:01:25 +0200

    Add homepage

--- ottilianer/config/urls.py
+++ ottilianer/config/urls.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.conf import settings
+from django.urls import include, re_path, path
+from wagtail import urls as wagtail_urls
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.documents import urls as wagtaildocs_urls
 
 urlpatterns = [
-    path("", include("ottilianer.home.urls")),
-    path("admin/", admin.site.urls),
+    path("django-admin/", admin.site.urls),
+    path("admin/", include(wagtailadmin_urls)),
+    path("documents/", include(wagtaildocs_urls)),
+    path("__reload__/", include("django_browser_reload.urls")),
+    re_path(r"", include("ottilianer.home.urls")),
+    re_path(r"", include(wagtail_urls)),
 ]
+
+if settings.DEBUG:
+    from django.conf import settings
+    from django.conf.urls.static import static
+
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
