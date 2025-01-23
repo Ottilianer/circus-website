7a60956d88077bc7c1501d18b1dcc08432292eb1
Leo Gall <gall.dev@proton.me>
2025-01-23 07:10:46 +0100

    fix import errors

--- server/handlers/presale/create.handler.ts
+++ server/handlers/presale/create.handler.ts
@@ -1,1 +1,45 @@
-export default () => {};
+export default (body: ) => {
+     // const { sendVerificationEmail } = usePresaleVerification();
+  // const { name, email, regularCards, discountCards, performance } =
+  //   await readBody(event);
+  // if (
+  //   !name ||
+  //   !email ||
+  //   regularCards === undefined ||
+  //   discountCards === undefined ||
+  //   !performance
+  // ) {
+  //   return constructErrorResponse(400, {
+  //     errorId: "MISSING_FIELDS",
+  //     message: "Missing required fields",
+  //   });
+  // }
+  // if (!isValidEmail(email)) {
+  //   return constructErrorResponse(400, {
+  //     errorId: "INVALID_EMAIL",
+  //     message: "Invalid email",
+  //   });
+  // }
+  // if (!isValidName(name)) {
+  //   return constructErrorResponse(400, {
+  //     errorId: "INVALID_NAME",
+  //     message: "Invalid name",
+  //   });
+  // }
+  // try {
+  //   await sendVerificationEmail({
+  //     name,
+  //     email,
+  //     regularCards,
+  //     discountCards,
+  //     performance,
+  //   });
+  // } catch (error) {
+  //   console.error(error);
+  //   return constructErrorResponse(500, {
+  //     errorId: "SEND_EMAIL_FAILED",
+  //     message: "Failed to send verification email",
+  //   });
+  // }
+  // return constructSuccessResponse(201, "Presale created");
+};
