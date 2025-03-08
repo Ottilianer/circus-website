2141480c6538012ad142653af2f3baca9190b99e
Leo Gall <gall.dev@proton.me>
2025-03-08 22:32:05 +0100

    update presale text

--- components/presale/PresaleText.vue
+++ components/presale/PresaleText.vue
@@ -6,8 +6,11 @@
 const title = "Vorverkauf - Tickets sichern!";
 
 const subtitle = `Bei unserem Vorverkauf können Sie Tickets vorbestellen und sich damit einen Platz in unseren legendären Vorstellungen sichern.
- Diese Tickets müssen min. 90 Minuten vor Vorstellungsbeginn an der Kasse abgeholt werden. 
- Zusätzlich halten wir allerdings auch Tickets an der Tageskasse für Sie bereit.
- Wir freuen uns auf Ihren Besuch!
-  `;
+Diese Tickets müssen min. 90 Minuten vor Vorstellungsbeginn an der Kasse abgeholt werden.
+Zusätzlich halten wir allerdings auch Tickets an der Tageskasse für Sie bereit. 
+
+Sollten Sie auf einen Rollstuhl angewiesen sein, bitten wir Sie uns zu kontaktieren, damit wir die nötigen Vorbereitungen treffen können.
+
+Wir freuen uns auf Ihren Besuch!
+`;
 </script>
