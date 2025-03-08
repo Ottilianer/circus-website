73e17a6eb27554ac075904b4847dea7c4ab1c0fe
Leo Gall <gall.dev@proton.me>
2025-03-08 22:28:40 +0100

    update texts

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -6,10 +6,10 @@
 </template>
 
 <script setup lang="ts">
-const title = "Galerie";
+const title = "Unsere Galerie - Rückblicke in die Vergangenheit";
 
 const subtitle =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+  "Tauchen Sie ein in die magische Welt des Circus St.Ottilien - Erleben Sie faszinierende Einblicke in die Manege der vergangenen Jahre und lassen Sie sich verzaubern von atemberaubenden Momenten, die einen Vorgeschmack auf das geben, was Sie im Jahr 2025 bei unserer spektakulären Show erwarten wird.";
 
 useHead({
   title: "Circus St. Ottilien • Galerie",
